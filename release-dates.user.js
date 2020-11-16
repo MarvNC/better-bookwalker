200c88cd8c12c0c69fe75415ebf09eccb9f4563e
MarvNC <rotrobmin@gmail.com>
2020-11-15 16:45:12 -0800

    change vers num

--- release-dates.user.js
+++ release-dates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Novel Stats Charts
 // @namespace    https://github.com/MarvNC
-// @version      0.1
+// @version      0.11
 // @description  A userscript that generates charts about novel series.
 // @author       Marv
 // @match        https://bookwalker.jp/series/*
