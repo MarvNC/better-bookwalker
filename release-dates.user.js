ef1bc0d3500da5e65e34651b377b61454539f1c4
MarvNC <rotrobmin@gmail.com>
2020-11-19 02:48:36 -0800

    Fix volRegex for .x titles

--- release-dates.user.js
+++ release-dates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Novel Stats Charts
 // @namespace    https://github.com/MarvNC
-// @version      0.28
+// @version      0.29
 // @description  A userscript that generates charts about novel series.
 // @author       Marv
 // @match        https://bookwalker.jp/series/*
@@ -17,7 +17,7 @@
 // @grant        GM_getResourceText
 // ==/UserScript==
 
-const volRegex = /([\d\.]+)/g;
+const volRegex = /(\d+\.?\d*)/g;
 const dayMs = 86400000;
 const monthMs = 2592000000;
 
