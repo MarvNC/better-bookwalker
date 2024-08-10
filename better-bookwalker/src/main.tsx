12c4e3bc38cc4ade4cc931eb5ba9f83b65aaddab
MarvNC <rotrobmin@gmail.com>
2024-08-09 20:36:53 -0700

    Prettier, Tailwind

--- better-bookwalker/src/main.tsx
+++ better-bookwalker/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(
   (() => {
-    const app = document.createElement('div');
+    const app = document.createElement("div");
     document.body.append(app);
     return app;
   })(),
--- better-bookwalker/vite.config.ts
+++ better-bookwalker/vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import monkey, { cdn } from 'vite-plugin-monkey';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import monkey, { cdn } from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     monkey({
-      entry: 'src/main.tsx',
+      entry: "src/main.tsx",
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        name: "Better Bookwalker",
+        icon: "https://vitejs.dev/logo.svg",
+        namespace: "https://github.com/MarvNC",
+        match: ["https://*.bookwalker.jp/*"],
       },
       build: {
         externalGlobals: {
-          react: cdn.jsdelivr('React', 'umd/react.production.min.js'),
-          'react-dom': cdn.jsdelivr(
-            'ReactDOM',
-            'umd/react-dom.production.min.js',
+          react: cdn.jsdelivr("React", "umd/react.production.min.js"),
+          "react-dom": cdn.jsdelivr(
+            "ReactDOM",
+            "umd/react-dom.production.min.js",
           ),
         },
       },
