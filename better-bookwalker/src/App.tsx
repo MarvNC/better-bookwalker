d2df1ffcd3bad5d65cd98a2b0b9f0d727c7aa13b
MarvNC <rotrobmin@gmail.com>
2024-08-10 00:28:34 -0700

    prettier

--- better-bookwalker/src/App.tsx
+++ better-bookwalker/src/App.tsx
@@ -19,9 +19,9 @@ export default function App() {
   if (!pageType) throw new Error("Unknown page type");
   return (
     <div className="rounded-md bg-sky-100 p-10 text-left font-sans">
-      <div className="max-w-[900px] mx-auto">
-      {currentPageType === pageType.series ? <Series /> : null}
-      {currentPageType === pageType.book ? <Book /> : null}
+      <div className="mx-auto max-w-[900px]">
+        {currentPageType === pageType.series ? <Series /> : null}
+        {currentPageType === pageType.book ? <Book /> : null}
       </div>
     </div>
   );
