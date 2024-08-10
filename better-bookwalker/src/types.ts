a1fd0db47599dcfa243f9a23c63d3585209211b3
MarvNC <rotrobmin@gmail.com>
2024-08-09 20:14:52 -0700

    Init vitemonkey

---
+++ better-bookwalker/src/types.ts
@@ -0,0 +1,85 @@
+type UUID = string;
+
+export type SeriesInfo = {
+  seriesId: number;
+  seriesName: string | null;
+  books: UUID[];
+  lastFetched: string;
+};
+
+export type SeriesInfoApiResponse = {
+  series_info: {
+    uuid: string;
+    thumbnail_id: string;
+    product_name_kana: string;
+    series_no: number;
+    moral_type_code: string;
+    deliveryServiceType: string[];
+    iOSNG: number;
+    message: string | null;
+    is_wauri: boolean;
+    productName: string;
+    productPrice: number;
+    saleEndTime: string | null;
+  }[];
+  update_date: string;
+};
+
+export type BookInfo = {
+  uuid: string;
+  title: string;
+  titleKana: string;
+  seriesIndex: number;
+  detailsShort: string;
+  details: string;
+  thumbnailImageUrl: string;
+  coverImageUrl: string;
+  releaseDate: string | null;
+};
+
+type Author = {
+  authorTypeName: string;
+  authorName: string;
+  authorNameKana: string;
+}[];
+
+export type BookApiResponse = {
+  productId: number;
+  productName: string;
+  productNameKana: string;
+  uuid: string;
+  authors: Author[];
+  moralTypeCode: string;
+  companyName: string;
+  copyRightString: string;
+  productExplanationShort: string;
+  productExplanationDetails: string;
+  productTypeCode: string;
+  productTypeName: string;
+  thumbnailImageUrl: string;
+  coverImageUrl: string;
+  licenceUnitUrl: string;
+  productVersionSys: number;
+  productVersionDisp: string | null;
+  fileVersion: number;
+  pdfFileTypes: string[];
+  twitterOutputFlag: boolean;
+  drmTimeLimit: string | null;
+  comicFlag: boolean;
+  sharedFileSize: number | null;
+  sharedExpandSize: number | null;
+  sharedFileVersion: number | null;
+  versionupLimitTime: string | null;
+  omfFlag: boolean;
+  labelId: number;
+  labelName: string;
+  seriesId: number;
+  seriesName: string;
+  seriesNameKana: string;
+  seriesNo: number;
+  categoryId: number;
+  categoryName: string;
+  bvFileVersion: number;
+  bvAudioVisualTypeCode: string;
+  bvOpenFlag: number;
+};
