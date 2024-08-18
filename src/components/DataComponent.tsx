7e7b7c9538d3523d88ff4f756ac348ce7d498c6c
MarvNC <rotrobmin@gmail.com>
2024-08-17 22:57:48 -0700

    Separate out to DataComponent

---
+++ src/components/DataComponent.tsx
@@ -0,0 +1,53 @@
+import { CalendarPlus, ChartLine, ChevronsUpDown } from "lucide-react";
+
+import { Series } from "@/utils/bookwalker/series";
+
+import { Button } from "./ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import { Input } from "./ui/input";
+
+export default function DataComponent({ series }: { series: Series | null }) {
+  return (
+    <>
+      {series && (
+        <div className="flex flex-col gap-2 rounded-lg bg-white p-4 text-sky-800 shadow-md">
+          <Collapsible>
+            <CollapsibleTrigger className="flex w-full cursor-pointer flex-row items-center justify-center gap-2 text-center text-2xl text-sky-800">
+              <span className="">Data Options</span>
+              <ChevronsUpDown />
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="mt-4 flex items-center justify-between gap-6">
+                <Button
+                  className="h-14 flex-1 px-5 py-3 text-xl"
+                  onClick={() => console.log("Add Release Prediction clicked")}
+                >
+                  <CalendarPlus className="mr-2 h-6 w-6" />
+                  Add Release Prediction
+                </Button>
+                <div className="flex flex-1 items-center gap-4">
+                  <Input
+                    className="h-14 flex-grow px-5 py-3 text-xl placeholder:text-slate-400"
+                    placeholder="https://global.bookwalker.jp/series/359330/"
+                    type="url"
+                  />
+                  <Button
+                    className="h-14 px-5 py-3 text-xl"
+                    onClick={() => console.log("Compare Series clicked")}
+                  >
+                    <ChartLine className="mr-2 h-6 w-6" />
+                    Compare
+                  </Button>
+                </div>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
+      )}
+    </>
+  );
+}
--- src/components/SeriesComponent.tsx
+++ src/components/SeriesComponent.tsx
@@ -1,6 +1,5 @@
 import "react-toastify/dist/ReactToastify.css";
 
-import { CalendarPlus, ChartLine, ChevronsUpDown } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 import BookGrid from "@/components/BookGrid";
@@ -9,13 +8,7 @@ import SeriesHeader from "@/components/SeriesHeader";
 import { ProcessedBookInfo, SeriesInfo } from "@/types";
 import { Series } from "@/utils/bookwalker/series";
 
-import { Button } from "./ui/button";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "./ui/collapsible";
-import { Input } from "./ui/input";
+import DataComponent from "./DataComponent";
 
 export default function SeriesComponent() {
   const [seriesInfo, setSeriesInfo] = useState<SeriesInfo | null>(null);
@@ -49,39 +42,8 @@ export default function SeriesComponent() {
         />
       )}
 
-      <div className="flex flex-col gap-2 rounded-lg bg-white p-4 text-sky-800 shadow-md">
-        <Collapsible>
-          <CollapsibleTrigger className="flex w-full cursor-pointer flex-row items-center justify-center gap-2 text-center text-2xl text-sky-800">
-            <span className="">Data Options</span>
-            <ChevronsUpDown />
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <div className="mt-4 flex items-center justify-between gap-6">
-              <Button
-                className="h-14 flex-1 px-5 py-3 text-xl"
-                onClick={() => console.log("Add Release Prediction clicked")}
-              >
-                <CalendarPlus className="mr-2 h-6 w-6" />
-                Add Release Prediction
-              </Button>
-              <div className="flex flex-1 items-center gap-4">
-                <Input
-                  className="h-14 flex-grow px-5 py-3 text-xl placeholder:text-slate-400"
-                  placeholder="https://global.bookwalker.jp/series/359330/"
-                  type="url"
-                />
-                <Button
-                  className="h-14 px-5 py-3 text-xl"
-                  onClick={() => console.log("Compare Series clicked")}
-                >
-                  <ChartLine className="mr-2 h-6 w-6" />
-                  Compare
-                </Button>
-              </div>
-            </div>
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
+      {/* Data Options */}
+      <DataComponent series={series} />
 
       {/* Book Grid */}
       <BookGrid booksInfo={booksInfo} />
