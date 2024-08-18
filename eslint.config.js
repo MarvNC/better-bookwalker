19a651da0031cc5cdc0033e9118d105224df4fa5
MarvNC <rotrobmin@gmail.com>
2024-08-17 21:44:22 -0700

    Add collapsible

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,12 @@ import tseslint from "typescript-eslint";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    ignores: ["src/components/ui/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
