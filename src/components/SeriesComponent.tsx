ea2714939eb20f91f0e47aba3721057da5d836e9
MarvNC <rotrobmin@gmail.com>
2024-08-17 22:39:44 -0700

    Add basic data options design

--- src/components/SeriesComponent.tsx
+++ src/components/SeriesComponent.tsx
@@ -1,6 +1,6 @@
 import "react-toastify/dist/ReactToastify.css";
 
-import { ChevronsUpDown } from "lucide-react";
+import { CalendarPlus, ChartLine, ChevronsUpDown } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 import BookGrid from "@/components/BookGrid";
@@ -9,11 +9,13 @@ import SeriesHeader from "@/components/SeriesHeader";
 import { ProcessedBookInfo, SeriesInfo } from "@/types";
 import { Series } from "@/utils/bookwalker/series";
 
+import { Button } from "./ui/button";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "./ui/collapsible";
+import { Input } from "./ui/input";
 
 export default function SeriesComponent() {
   const [seriesInfo, setSeriesInfo] = useState<SeriesInfo | null>(null);
@@ -53,7 +55,28 @@ export default function SeriesComponent() {
             <span className="">Data Options</span>
             <ChevronsUpDown />
           </CollapsibleTrigger>
-          <CollapsibleContent></CollapsibleContent>
+          <CollapsibleContent className="mt-4 flex flex-col gap-4">
+            <div className="flex items-center justify-between gap-4">
+              <Button
+                className="flex-1"
+                onClick={() => console.log("Add Release Prediction clicked")}
+              >
+                <CalendarPlus className="mr-2 h-4 w-4" />
+                Add Release Prediction
+              </Button>
+              <div className="flex flex-1 items-center gap-2">
+                <Input
+                  className="flex-grow"
+                  placeholder="https://global.bookwalker.jp/series/359330/"
+                  type="url"
+                />
+                <Button onClick={() => console.log("Compare Series clicked")}>
+                  <ChartLine className="mr-2 h-4 w-4" />
+                  Compare
+                </Button>
+              </div>
+            </div>
+          </CollapsibleContent>
         </Collapsible>
       </div>
 
